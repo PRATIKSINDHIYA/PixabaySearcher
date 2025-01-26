7943ee4310661b49b42c0fe4eb2ea086b8a79cae
PRATIKSINDHIYA <pratiksindhiya3@gmail.com>
2025-01-27 02:40:29 +0530

    add loading function

--- src/context/ImageState.jsx
+++ src/context/ImageState.jsx
@@ -4,23 +4,29 @@ import ImageContext from './ImageContext'
 const ImageState = (props) => {
     const apikey = '48187514-953107941c7a53e55110e24cd'
     const [imagedata, setimagedata] = useState([])
+    const [loading, setloading] = useState(false)
     const [query, setQuery] = useState('india')
+
     useEffect(() => {
+        setloading(true)
         const fetchapi = async () => {
             const api = await fetch(`https://pixabay.com/api/?key=${apikey}&q=${query}&image_type=photo&per_page=100`)
             const data = await api.json()
             setimagedata(data.hits)
+            setloading(false)
         }
         fetchapi()
+        setloading(false)
     },[query])
 
     const Fetchimagebycategory = async(category) => {
+      setloading(true)
         const api = await fetch(`https://pixabay.com/api/?key=${apikey}&category=${category}&image_type=photo&per_page=100`)
         const data = await api.json()
-        setimagedata(data.hits)
-      
-    };
+        setimagedata(data.hits)  
+        setloading(false)
 
+    };
   return (
     <ImageContext.Provider value={{imagedata,Fetchimagebycategory,setQuery}}>
       {props.children}
